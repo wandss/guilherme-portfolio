ee6ff72be5de076784ac751f8955873b18ef0e7f
WANDERLEY DE SOUZA SILVA <26432@copasa.com.br>
2018-09-12 14:18:55 -0300

    Create custom checkbox component

---
+++ src/components/AppCheckbox.vue
@@ -0,0 +1,54 @@
+<template>
+    <div class="form-group">
+        <input type="checkbox" :id="label"
+         @input="$emit('input', $event.target.checked)"
+        />
+        <label :for="label"> {{label}}</label>
+    </div>
+</template>
+<script>
+export default{
+    name:'AppCheckbox',
+    props:{
+        label:{
+            type:String,
+            default:'Label Name Here',
+            required:true,
+        }
+    }
+}
+</script>
+<style scoped>
+input{
+    appearance:none;
+    outline:none;
+    display:inline-block;
+    margin-right:.5rem;
+    background-color:#fafafa;
+    border:solid 1px #cacece;
+    height:1rem;
+    width:1rem;
+    box-shadow: 0 1px 2px rgba(0,0,0,0.05), inset 0px -15px 10px -12px rgba(0,0,0,0.05);
+    position:relative;
+}
+input:checked, input:checked:active{
+    box-shadow: 0 1px 2px rgba(0,0,0,0.05), inset 0px 1px 3px rgba(0,0,0,0.1);
+}
+input:checked{
+    background-color: #e9ecee;
+    border: 1px solid #adb8c0;
+    box-shadow: 0 1px 2px rgba(0,0,0,0.05), inset 0px -15px 10px -12px rgba(0,0,0,0.05), inset 15px 10px -12px rgba(255,255,255,0.1);
+    color: #99a1a7;
+}
+input:checked:after{
+    content: '\2714';
+    font-size: 1.8rem;
+    position: absolute;
+    top:-1.3rem;
+    left:-.1rem;
+    /*color: #99a1a7;*/
+    /*color:#56ad79;*/
+    color:rgba(76, 196, 102, 1);
+    position:absolute;
+}
+</style>
--- src/components/crud/CreateAlbum.vue
+++ src/components/crud/CreateAlbum.vue
@@ -2,26 +2,48 @@
     <Modal :show="show" modalSize="modal-full" title="Criar Album"
      @click="$emit('close')" >
         <div slot="body">
-            Form
-            <hr/>
-            Foto da Capa:
-            Após Clicar na foto alterar nome para:
-            Selecionar fotos.
-            <hr/>
+            <form>
+                <div class="row">
+                    <div class="col">
+                        <app-input type="text" required label="Nome do Album:"
+                         v-model="name" placeholder="Nome do Album"/>
+                        <app-input type="text" required label="Descição:"
+                         v-model="description" placeholder="Descrição"/>
+                        <app-checkbox v-model="publish.checked" 
+                         :label="publish.label" />
+                        <app-checkbox v-model="public.checked" 
+                         :label="public.label" />
+                    </div>
+                </div>
+                <hr/>
+                Foto da Capa:
+                Após Clicar na foto alterar nome para:
+                Selecionar fotos.
+                <hr/>
+            </form>
         </div>
     </Modal>
 </template>
 <script>
 import Modal from '@/components/Modal';
+import AppCheckbox from '@/components/AppCheckbox';
 
 export default{
     name:'CreateAlbum',
-    components:{Modal,},
+    components:{Modal, AppCheckbox},
     props:{
         show:{
             type:Boolean,
             default:false,
             required:true
+        }
+    },
+    data(){
+        return{
+            name:null,
+            description:null,
+            publish:{label:'Publicar',checked:false},
+            public:{label:'Público',checked:false},
         }
     }
 }
